1abbb70f91cf57717c113eda64d148865fa6903a
Dan Fortinho <fortinhodanvhamba@gmail.com>
2024-12-06 13:09:15 +0200

    ajout de controllers, models, middleware

--- src/index.js
+++ src/index.js
@@ -17,4 +17,16 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 
+app.get('/', async (req, res, next) => {
+    res.send('WELCOME TO OUR GARAGE MANAGEMENT API !!!')
+})
+
+app.use('/api-type-user', require('./routes/typeUser'))
+app.use('/api-user', require('./routes/user'))
+app.use('/api-category', require('./routes/category'))
+app.use('/api-requerant', require('./routes/requerant'))
+app.use('/api-specimen', require('./models/specimen'))
+
+
+
 app.listen(port, () => console.log(`Server started on port ${port}`))
